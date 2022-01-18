680361a5b8b12f20ea2000eb6f3d732c2cd9e151
SammyG1996 <sgonzalezjr1914@gmail.com>
2022-01-18 15:47:17 -0500

    updated tests

--- calculator-test.js
+++ calculator-test.js
@@ -1,13 +1,18 @@
 
-it('should calculate the monthly rate correctly', function () {
-  expect(calculateMonthlyPayment(5000, 1, 3.5)).toEqual('424.61')
-});
+describe('calculator rate test', () => {
+  it('should calculate the monthly rate correctly', function () {
+    expect(calculateMonthlyPayment(5000, 1, 3.5)).toEqual('424.61')
+  });
+})
+
+describe('calculator output tests', () => {
+  it("should return a result with 2 decimal places", function() {
+    expect(calculateMonthlyPayment(1000, 2, 3.5)).toBeCloseTo(43.2, 2)
+  });
+  
+  it('should return a string', () => {
+    expect(typeof(calculateMonthlyPayment(1000, 2, 3.5))).toBe('string')
+  });
+})
 
 
-it("should return a result with 2 decimal places", function() {
-  expect(calculateMonthlyPayment(1000, 2, 3.5)).toBeCloseTo(43.2, 2)
-});
-
-it('should return a string', () => {
-  expect(typeof(calculateMonthlyPayment(1000, 2, 3.5))).toBe('string')
-});
